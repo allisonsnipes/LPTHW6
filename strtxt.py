7d18856164f9fa2b7270303ddadb056133fd0cef
Allison Snipes <ajsnipes@usfca.edu>
2019-11-11 15:11:51 -0800

    refactored code

--- strtxt.py
+++ strtxt.py
@@ -1,7 +1,20 @@
-x = 'there are %d types of people' % 10
+x = "there are %d types of people." % 10
 binary = 'binary'
 do_not = 'dont'
-y = x = 'those who know %s and those who %s.' % (binary, do_not)
+y = 'those who know %s and those who %s.' % (binary, do_not)
 
 print x
 print y
+
+print 'i said %r' % x
+print 'i said %s' % y
+
+hilarious = False
+joke_evaluation = 'isnt that joke so %r'
+
+print joke_evaluation % hilarious
+
+w = 'this is the left side'
+e = 'this is the right side'
+
+print w + e
